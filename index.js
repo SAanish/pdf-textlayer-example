d0eb1a4e850f09afdf69e94ece379f14390237e4
Matt Zabriskie <mzabriskie@gmail.com>
2016-03-11 17:29:12 -0700

    Load all the pages

--- index.js
+++ index.js
@@ -1,22 +1,73 @@
+let pdfDocument;
+let PAGE_HEIGHT;
+const DEFAULT_SCALE = 1.33;
+
 PDFJS.workerSrc = './pdf.worker.js';
 PDFJS.getDocument('example.pdf').then(pdf => {
-  pdf.getPage(1).then(page => {
-    let canvas = document.getElementById('canvas');
-    let container = document.getElementById('container');
+  pdfDocument = pdf;
+
+  let viewer = document.getElementById('viewer');
+  for (let i=0; i<pdf.pdfInfo.numPages; i++) {
+    let page = createEmptyPage(i+1);
+    viewer.appendChild(page);
+  }
+
+  loadPage(1).then(pdfPage => {
+    let viewport = pdfPage.getViewport(DEFAULT_SCALE);
+    PAGE_HEIGHT = viewport.height;
+    document.body.style.width = `${viewport.width}px`;
+  });
+});
+
+window.addEventListener('scroll', handleWindowScroll);
+
+function createEmptyPage(num) {
+  let page = document.createElement('div');
+  let canvas = document.createElement('canvas');
+  let wrapper = document.createElement('div');
+  let textLayer = document.createElement('div');
+
+  page.className = 'page';
+  wrapper.className = 'canvasWrapper';
+  textLayer.className = 'textLayer';
+
+  page.setAttribute('id', `pageContainer${num}`);
+  page.setAttribute('data-loaded', 'false');
+  page.setAttribute('data-page-number', num);
+
+  canvas.setAttribute('id', `page${num}`);
+
+  page.appendChild(wrapper);
+  page.appendChild(textLayer);
+  wrapper.appendChild(canvas);
+
+  return page;
+}
+
+function loadPage(pageNum) {
+  return pdfDocument.getPage(pageNum).then(pdfPage => {
+    let page = document.getElementById(`pageContainer${pageNum}`);
+    let canvas = page.querySelector('canvas');
+    let wrapper = page.querySelector('.canvasWrapper');
+    let container = page.querySelector('.textLayer');
     let canvasContext = canvas.getContext('2d');
-    let viewport = page.getViewport(1);
+    let viewport = pdfPage.getViewport(DEFAULT_SCALE);
 
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
-    container.style.width = viewport.width + 'px';
-    container.style.height = viewport.height + 'px';
+    canvas.width = viewport.width * 2;
+    canvas.height = viewport.height * 2;
+    page.style.width = `${viewport.width}px`;
+    page.style.height = `${viewport.height}px`;
+    wrapper.style.width = `${viewport.width}px`;
+    wrapper.style.height = `${viewport.height}px`;
+    container.style.width = `${viewport.width}px`;
+    container.style.height = `${viewport.height}px`;
 
-    page.render({
+    pdfPage.render({
       canvasContext,
       viewport
     });
 
-    page.getTextContent().then(textContent => {
+    pdfPage.getTextContent().then(textContent => {
       PDFJS.renderTextLayer({
         textContent,
         container,
@@ -24,5 +75,19 @@ PDFJS.getDocument('example.pdf').then(pdf => {
         textDivs: []
       });
     });
+
+    page.setAttribute('data-loaded', 'true');
+
+    return pdfPage;
   });
-});
+}
+
+function handleWindowScroll() {
+  let visiblePageNum = Math.round(window.scrollY / PAGE_HEIGHT) + 1;
+  let visiblePage = document.querySelector(`.page[data-page-number="${visiblePageNum}"][data-loaded="false"]`);
+  if (visiblePage) {
+    setTimeout(function () {
+      loadPage(visiblePageNum);
+    });
+  }
+}
